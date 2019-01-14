cf6eb46c966e401de887360ab1aeec8a47b71808
Dayud Moahamed <voiphandy@gmail.com>
2019-01-14 10:33:04 +0300

    Add delete quote confirmation from the user before deleted.

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef){ }
+  
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
 
+  }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,9 +15,12 @@ export class QuoteComponent implements OnInit {
   ]
 
   
-  completeQuote(isComplete,index){
+  deleteQuote(isComplete,index){
     if (isComplete){
+      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].quote}`)
+      if(toDelete) {
         this.quotes.splice(index,1);
+      }  
         }
         }
   constructor() { }
